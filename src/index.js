37d1c104ec9e6140e157faef108f7896ee9ab947
Tomohiko Ozawa <kota65535@gmail.com>
2023-01-04 23:01:55 +0900

    lint & format, etc (#34)

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-const core = require('@actions/core')
-const coreCommand = require('@actions/core/lib/command')
-const main = require('./main')
-const post = require('./post')
+const core = require("@actions/core");
+const coreCommand = require("@actions/core/lib/command");
+const main = require("./main");
+const post = require("./post");
 
-const isPost = !!process.env.STATE_isPost
+const isPost = !!process.env.STATE_isPost;
 
 if (isPost) {
   // cleanup
-  const pid = process.env.STATE_pid
+  const pid = process.env.STATE_pid;
   try {
-    post(pid)
+    post(pid);
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 } else {
   // main
   try {
-    main(pid => coreCommand.issueCommand('save-state', { name: 'pid' }, pid))
+    main((pid) => coreCommand.issueCommand("save-state", { name: "pid" }, pid));
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   } finally {
     // cf. https://github.com/actions/checkout/blob/main/src/state-helper.ts
-    coreCommand.issueCommand('save-state', { name: 'isPost' }, 'true')
+    coreCommand.issueCommand("save-state", { name: "isPost" }, "true");
   }
 }
