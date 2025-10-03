3738dfd46d8b1c08e7a119d23026d58ab41e0795
Anderson Leite <anderson.leite@movingimage.com>
2025-10-03 11:36:15 +0100

    First try - Add support to openvpn v3
    Signed-off-by: Anderson Leite <anderson.leite@movingimage.com>

--- src/main.js
+++ src/main.js
@@ -92,7 +92,7 @@ const run = (callback) => {
     const tail = new Tail("openvpn.log");
 
     try {
-      exec(`sudo openvpn --config ${configFile} --daemon --log openvpn.log --writepid openvpn.pid`);
+      exec(`sudo openvpn2 --config ${configFile} --daemon --log openvpn.log --writepid openvpn.pid`);
     } catch (error) {
       core.error(fs.readFileSync("openvpn.log", "utf8"));
       tail.unwatch();
